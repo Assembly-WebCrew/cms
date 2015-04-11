127829754168091c9be392f0e86afb896199b4bd
Joona Romppanen <muta@pingtimeout.net>
2015-04-12 01:00:30 +0300

    Added .pid to ignore. Added tweetembly app

--- assembly/celery.py
+++ assembly/celery.py
@@ -11,7 +11,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'assembly.settings.local')
 app = Celery('assembly')
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
-
-@app.task(bind=True)
-def debug_task(self):
-    print('Request: {0!r}'.format(self.request))
--- assembly/settings/base.py
+++ assembly/settings/base.py
@@ -15,6 +15,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'djcelery',
     'rest_framework',
     'reversion',
     'mptt',
