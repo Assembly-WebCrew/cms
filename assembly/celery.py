85ecaa370dba3833273e0dd6c39ec1695cc7293c
Joona Romppanen <joona.romppanen@gmail.com>
2015-07-23 16:10:03 +0300

    Move schedule sync to celery.py

--- assembly/celery.py
+++ assembly/celery.py
@@ -11,3 +11,14 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'assembly.settings.local')
 app = Celery('assembly')
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+import requests
+import json
+
+@app.task
+def fetch_schedule():
+    r = requests.get('http://schedule.assembly.org/asms15/schedules/events.json')
+    if (r.status_code == 200):
+        data = r.json()
+        with open('./media/uploads/schedule/events.json', 'w') as f:
+            json.dump(data, f)
