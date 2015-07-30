4cd0ed780aa23d05406654abb481b976c05a653a
unknown <JoonaR@ICTL106.turvatiimi.local>
2015-07-30 13:17:44 +0300

    Removed hardcoded credentials. Moved to settings and updated local.py.template accordingly

--- tweetembly/tasks.py
+++ tweetembly/tasks.py
@@ -1,12 +1,19 @@
 from __future__ import absolute_import
 from celery import shared_task
+from django.conf import settings
 import tweepy
 
+auth = tweepy.OAuthHandler(settings.TWITTER.CONSUMER_KEY, settings.TWITTER.CONSUMER_SECRET)
+auth.set_access_token(settings.TWITTER.ACCESS_TOKEN, settings.TWITTER.ACCESS_SECRET)
+
+api = tweepy.API(auth)
+
+
 @shared_task
-def fetch_schedule(bind=True):
-    auth = tweepy.OAuthHandler("D42C4ev1mfGoKWHWiuQng4Uha", "OOL8tnnyQiMhNowPJ9Xhp1z85jvZd2isW6pDgdxZxgLbMwRzIb")
-    auth.set_access_token("250732849-6FulPWJtvIj28t5SoJiVAICw2pY4x1DjQmYeyubh", "aD4Z3DXvPDXsDzQlyEj719leJQFS3wUtCbdWqcw8tWkVO")
-
-    api = tweepy.API(auth)
-
+def fetch_timeline(bind=True):
     api.home_timeline()
+
+
+@shared_task
+def fetch_user_timeline(user_id):
+    pass
