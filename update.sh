23141a40b637a454e93c401460c31f54eb3307da
Felix Ippolitov <fellu@skyred.fi>
2015-08-01 18:54:53 +0300

    Added git submodule support to update script

--- update.sh
+++ update.sh
@@ -3,6 +3,10 @@ echo "Backing up current state"
 current_time=$(date "+%d%m%Y_%H%M%S")
 file_name='preupdate_state_'$current_time'.tar.gz'
 tar -zcvf /data/httpd/site/neo.assembly.org/$file_name /data/httpd/site/neo.assembly.org/staging/ &> /dev/null
+echo "Initialize and update submodules"
+git submodule init
+git submodule update
+git submodule status
 echo "Pulling repository"
 git pull
 echo "Cleaning statics"
