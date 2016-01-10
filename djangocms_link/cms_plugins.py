57cc123017bf9017c87803d21e1523e1ce21f403
joona romppanen <joona.romppanen@gmail.com>
2016-01-10 18:40:05 +0200

    Hotfix: Customise cmsplugin_link to return slugged url for internal pages

---
+++ djangocms_link/cms_plugins.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function, unicode_literals
+
+from cms.plugin_base import CMSPluginBase
+from cms.plugin_pool import plugin_pool
+from django.conf import settings
+from django.contrib.sites.models import Site
+from django.utils.translation import ugettext_lazy as _
+
+from djangocms_link.forms import LinkForm
+from djangocms_link.models import Link
+
+
+class LinkPlugin(CMSPluginBase):
+
+    model = Link
+    form = LinkForm
+    name = _('Link')
+    render_template = 'cms/plugins/link.html'
+    text_enabled = True
+    allow_children = True
+
+    def render(self, context, instance, placeholder):
+        link = instance.link()
+        context.update({
+            'name': instance.name,
+            'link': link,
+            'target': instance.target,
+            'placeholder': placeholder,
+            'object': instance
+        })
+        return context
+
+    def get_form(self, request, obj=None, **kwargs):
+        form_class = super(LinkPlugin, self).get_form(request, obj, **kwargs)
+
+        if obj and obj.page and obj.page.site:
+            site = obj.page.site
+        elif self.page and self.page.site:
+            site = self.page.site
+        else:
+            # this might NOT give the result you expect
+            site = Site.objects.get_current()
+
+        class Form(form_class):
+            def __init__(self, *args, **kwargs):
+                super(Form, self).__init__(*args, **kwargs)
+                self.for_site(site)
+
+        return Form
+
+    def icon_src(self, instance):
+        return '{0}cms/img/icons/plugins/link.png'.format(settings.STATIC_URL)
+
+plugin_pool.register_plugin(LinkPlugin)
---
+++ djangocms_link/forms.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function, unicode_literals
+
+from django.forms import ValidationError
+from django.forms.models import ModelForm
+from django.forms.widgets import Media
+from django.utils.translation import ugettext_lazy as _
+
+from djangocms_link.models import Link
+
+
+class LinkForm(ModelForm):
+    try:
+        from djangocms_link.fields import PageSearchField
+        page_link = PageSearchField(label=_('Page'), required=False)
+    except ImportError:
+        from cms.forms.fields import PageSelectFormField
+        page_link = PageSelectFormField(label=_('Page'), required=False)
+
+    def for_site(self, site):
+        # override the page_link fields queryset to contains just pages for
+        # current site
+        # this will work for PageSelectFormField
+        from cms.models import Page
+        self.fields['page_link'].queryset = Page.objects.drafts().on_site(site)
+        # set the current site as a page_link field instance attribute
+        # this will be used by the field later to properly set up the queryset
+        # this will work for PageSearchField
+        self.fields['page_link'].site = site
+
+    class Meta:
+        model = Link
+        exclude = ('page', 'position', 'placeholder', 'language', 'plugin_type')
+
+    def _get_media(self):
+        """
+        Provide a description of all media required to render the widgets on this form
+        """
+        media = Media()
+        for field in self.fields.values():
+            media = media + field.widget.media
+        media._js = ['cms/js/libs/jquery.min.js'] + media._js
+        return media
+    media = property(_get_media)
+
+    def clean(self):
+        cleaned_data = super(LinkForm, self).clean()
+        url = cleaned_data.get('url')
+        page_link = cleaned_data.get('page_link')
+        mailto = cleaned_data.get('mailto')
+        phone = cleaned_data.get('phone')
+        anchor = cleaned_data.get('anchor')
+        if not any([url, page_link, mailto, phone, anchor]):
+            raise ValidationError(_('At least one link is required.'))
+        return cleaned_data
